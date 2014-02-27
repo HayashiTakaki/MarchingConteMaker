03d90ff4eb2cb69a6361fa97c24c78b4e29092e8
HayashiTakaki <minidrasub@gmail.com>
2014-02-27 15:09:13 +0900

    no2

---
+++ no2/no2-b.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include "stack.cpp"
+using namespace std;
+
+
+template <class T>
+class queue {
+	
+private:
+	int size_of_q;
+	stack<T> Q;
+	stack<T> temp;
+	
+public:
+	queue(){
+		size_of_q = 0;
+	}
+	
+	
+	void add(T);
+	T peek();
+	T remove();
+	int size();
+	
+	
+	void show(){
+		cout << "Q-" << Q.size << endl;
+		cout << "temp-" << temp.size << endl;
+		
+		
+		
+	}
+	
+	
+	
+};
+
+
+
+template<class T> T queue<T>::peek(){
+	if (!Q.empty()) {
+		cout << "stack is empty" << endl;
+		exit(2);
+	}
+	
+	return Q.peek();
+}
+
+template<class T> T queue<T>::remove(){
+	if (!Q.empty()) {
+		cout << "stack is empty" << endl;
+		exit(2);
+	}
+	
+	return Q.pop();
+}
+
+template<class T> void queue<T>::add(T element){
+	while (!Q.empty()) {
+		cout << "Qpop tmppush "<< endl;
+		temp.push(Q.pop());
+	}
+	cout << "Qpush" << endl;
+	Q.push(element);
+	
+	while (!temp.empty()) {
+		cout << "tmpop Qpush "<< endl;
+
+		Q.push(temp.pop());
+	}
+	
+	return; 
+}
+
+template<class T> int queue<T>::size(){
+	return Q.size();
+}
+
