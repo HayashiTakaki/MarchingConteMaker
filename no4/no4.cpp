e548b4bc956055b13ce49cc64d33e1fe9cf8e2d4
HayashiTakaki <minidrasub@gmail.com>
2014-02-28 09:56:25 +0900

    no4

--- no4/no4.cpp
+++ no4/no4.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 
 int minus(int a, int b){
-	
+	int n;
 	if (a > b) {
+		
 		//a+n=bになる数を探索
-		for (int n = 0; n < b; n++) {
+		for (n = 0; n <= abs(a)+abs(b); n++) {
 			if (b + n == a) {
 				return n;
 			}
 		}
 	}else {
-		//a+n=bになる数を探索
-		for (int n = 0; n < b; n += (-1)) {
+
+		//b+n=aになる数を探索
+		for (n = 0; n <= abs(a)+abs(b); n += (-1)) {
 			if (b + n == a) {
 				return n;
 			}
@@ -29,8 +31,11 @@ int multiply(int a, int b){
 		sum += a;
 	}
 	
+	
 	//符号の調整
 	if (a < 0 && b < 0) {
+		sum = minus(0, sum);
+	}else if (b < 0) {
 		sum = minus(0, sum);
 	}
 	
@@ -39,13 +44,31 @@ int multiply(int a, int b){
 
 int divide(int a, int b){
 	int ans = 0;
-	while (a > 0) {
-		a = minus(a, b);
+	int temp = abs(a);
+	
+	
+	//0除算
+	if (b == 0) {
+		printf("error: divided by 0\n");
+		exit(2);
 	}
-	ans += 1;
+	
+	//a<bならば0を返却
+	if (abs(a) < abs(b)) {
+		return 0;
+	}
+	
+	//a<bになるまでa-bを繰り返す
+	while (temp > 0) {
+		temp = minus(temp, abs(b));
+		ans += 1;
+	}
+	ans = minus(ans, 1);
+	
+	
 	
 	//符号の調整
-	if (a < 0 && b < 0) {
+	if (a < 0 && b > 0  ||  a > 0 && b < 0) {
 		ans = minus(0, ans);
 	}
 	
@@ -56,11 +79,32 @@ int divide(int a, int b){
 int main (int argc, char * const argv[]) {
 	int a,b;
 	a = 5;
-	b = 3;
+	b = 10;
 	
+	printf("a = %d, b = %d\n",a, b);
 	printf("minus %d\n",minus(a, b));
 	printf("mul %d\n",multiply(a, b));
-	printf("div %d\n",divide(a, b));
+	printf("div %d\n\n",divide(a, b));
+	
+
+	
+	printf("a = %d, b = %d\n",-a, b);
+	printf("minus %d\n",minus(-a, b));
+	printf("mul %d\n",multiply(-a, b));
+	printf("div %d\n\n",divide(-a, b));
+	
+	
+	
+	printf("a = %d, b = %d\n",a, -b);
+	printf("minus %d\n",minus(a, -b));
+	printf("mul %d\n",multiply(a, -b));
+	printf("div %d\n\n",divide(a, -b));
+	
+	
+	printf("a = %d, b = %d\n",-a, -b);
+	printf("minus %d\n",minus(-a, -b));
+	printf("mul %d\n",multiply(-a, -b));
+	printf("div %d\n\n",divide(-a, -b));
 	
 	return 0;
 }
